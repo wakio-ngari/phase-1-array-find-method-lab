40a0909bc8e4784a43420886f6fefd2c93574223
wakio-ngari <ngarijoyce017@gmail.com>
2024-12-05 15:28:58 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record){
+    const winner =record.find(game =>game.result ==="W");//if you check for the losing year and replace the "w" with "l" youll get the year they lost
+       return winner?winner.year :undefined;
+ }
+ const record = [
+     { year: "2016", result: "N/A"},
+     { year: "2015", result: "W"},
+     { year: "2013", result: "L"},
+     
+   ];
+   console.log(superbowlWin(record));
+   console.log(superbowlWin(record,));
